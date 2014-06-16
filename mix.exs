6f01c4cfeb14751da9c858dd21536f0dcdb81cab
David Rouchy <drouchy@gmail.com>
2014-06-16 18:59:51 +0100

    fix compilation on 0.14.0 - tests are still failing

--- mix.exs
+++ mix.exs
@@ -7,20 +7,18 @@ defmodule Mock.Mixfile do
       source_url: "https://github.com/jjh42/mock",
       homepage_url: "http://jjh42.github.io/mock",
       version: "0.0.3",
-      env: [
-          dev: [deps: deps ++ dev_deps],
-          test: [deps: deps] ,
-          prod: [deps: deps]]]
+      deps: deps(Mix.env)
+    ]
   end
 
   # Returns the list of dependencies in the format:
   # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
-  defp deps do
-    [ {:meck,"0.7.2", [github: "eproxus/meck", tag: "0.7.2"]}]
+  # Dependencies only needed during development.
+  defp deps(:dev) do
+    [{ :docs_ghpages, github: "jjh42/docs_ghpages" }]
   end
 
-  # Dependencies only needed during development.
-  defp dev_deps do
-    [{ :docs_ghpages, github: "jjh42/docs_ghpages" }]
+  defp deps(_) do
+    [ {:meck,"0.8.2", [github: "eproxus/meck", tag: "0.8.2"]}]
   end
 end
